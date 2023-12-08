cdcce86d293813484e59381b1d537e9e4b7c61d8
Meron Kedir Esatu <88824826+Meron-kedir@users.noreply.github.com>
2023-12-09 02:34:04 +0300

    Update Hero.tsx

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -3,7 +3,7 @@
 function Hero() {
   return (
     // <div className=" flex justify-between grid-cols-1 md:grid-cols-3">
-    <div className='  bg-[url("public/hero-bg.png")] h-screen bg-cover bg-no-repeat bg-center margin-top: -56px; p-20 py-40'>
+    <div className='  bg-[url("/hero-bg.png")] h-screen bg-cover bg-no-repeat bg-center margin-top: -56px; p-20 py-40'>
       <div className="flex flex-col col-span-2 items-center justify-center">
         <h1 className="mb-2 text-8xl font-bold text-center text-white">
           Welcome to DOMFORTRA
@@ -27,14 +27,14 @@ function Hero() {
         </div>
         {/* <div className="flex flex-row items-end justify-end py-3">
           <img
-            src="public/hero-img.svg"
+            src="/hero-img.svg"
             alt="hero 2"
             style={{ width: "300px", height: "200px" }}
           />
         </div> */}
 
         {/* <div className="flex flex-col items-center py-3">
-          <img src="public/Animation - 1701845456553.json" alt="image" />
+          <img src="/Animation - 1701845456553.json" alt="image" />
         </div> */}
       </div>
     </div>
