e478483094588c8ccb0c86736e152210e8cd31e7
Meron Kedir Esatu <88824826+Meron-kedir@users.noreply.github.com>
2023-12-09 02:28:31 +0300

    Update Footer.tsx

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -9,7 +9,7 @@ function Footer() {
             <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
               <div className="md:flex md:justify-between">
                 <div className="mb-6 md:mb-0">
-                  <a href="public/loogo2.png" className="flex items-center">
+                  <a href="/loogo2.png" className="flex items-center">
                     <img
                       src="public/Group 1000003637 (1).png"
                       className="h-8 mr-3 "
